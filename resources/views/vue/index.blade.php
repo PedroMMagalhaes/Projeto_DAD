1c7d5f02364de3318566f0e38d7e41b0907c1b07
Pedro M <pedromagtaem@gmail.com>
2019-11-26 04:46:12 +0000

    IncludeUserNavbar

--- resources/views/vue/index.blade.php
+++ resources/views/vue/index.blade.php
@@ -8,7 +8,9 @@
     <router-link to="/profile">Profile</router-link> -
     <router-link to="/register">Register</router-link> -
     <router-link to="/login">Login</router-link> -
-    <router-link to="/logout">Logout</router-link> 
+    <router-link to="/logout">Logout</router-link> -
+    |<em>User: @{{this.$store.state.user != null ? this.$store.state.user.name : " - None - " }}</em>|
+    <hr>
     <router-view></router-view>
 @endsection
 
